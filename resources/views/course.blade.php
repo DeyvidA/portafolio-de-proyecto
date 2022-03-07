c6d738d38baaab5502b1cdf69ffe8dca2053c785
DeyvidA <deyvida1001@gmail.com>
2022-03-07 10:39:44 -0600

    Index y layout

---
+++ resources/views/course.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.web')
+
+@section('content')
+    Detalle del curso
+
+    <div class="text-center">
+        <h1 class="text-3xl text-gray-700 mb-2 uppercase">Ultimas noticias</h1>
+        <h2 class="text-xl text-gray-600">La nueva pelicual de jujutsu Kaisen esta por lasi</h2>
+
+    </div>
+
+    <livewire:course-list>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.web')
+
+@section('content')
+    <div class="text-center">
+        <h1 class="text-3xl text-gray-700 mb-2 uppercase">Ultimas noticias</h1>
+        <h2 class="text-xl text-gray-600">La nueva pelicual de jujutsu Kaisen esta por lasi</h2>
+        <h3 class="text-lg text-gray-600">El 90% de los televidentes de japon fueron a ver el dia de estreno</h3>
+    </div>
+
+    <livewire:course-list>
+@endsection
